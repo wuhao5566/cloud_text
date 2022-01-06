3581344815bf7f86dee2563af29fddb8b4202329
wuhao5566 <badboyswuhao@163.com>
2022-01-06 17:38:45 +0800

    第二次提交

---
+++ gateway-test/src/main/java/com/xyu/gateway/gatewayApplication.java
@@ -0,0 +1,13 @@
+package com.xyu.gateway;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+public class gatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(gatewayApplication.class,args);
+    }
+}
